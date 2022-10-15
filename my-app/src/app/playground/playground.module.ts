009e37ef2b03dd0c33cef4e57c7c2ff75cf696b4
daniel <daniel@daniels-MacBook-Pro.local>
2022-10-15 16:22:15 -0700

    daniel

--- my-app/src/app/playground/playground.module.ts
+++ my-app/src/app/playground/playground.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { PlaygroundComponent } from './playground.component';
 import { PlaygroundRoutingModule } from './playground-routing.module'
-
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -13,8 +13,9 @@ import { PlaygroundRoutingModule } from './playground-routing.module'
 ],
   imports: [
     CommonModule,
+    FormsModule
   ],
-  providers: [PlaygroundRoutingModule]
+  providers: [PlaygroundRoutingModule,]
 
 })
 export class PlaygroundModule { }
