e1992dd9c773f42ad5f486db332dc1054f38c8c8
Matt Tsai <asd881018@yahoo.com.tw>
2022-10-15 16:48:03 -0700

    modify route

--- my-app/src/events.js
+++ my-app/src/events.js
@@ -5,10 +5,12 @@ function createRouter(db) {
   const owner = '';
 
   // the routes are defined here
-  router.post('/event', (req, res, next) => {
+
+  // Get all the scoreboard by desc order
+  router.get('/event', (req, res, next) => {
     db.query(
-      // "SELECT userName, score FROM scoreboard order by score DESC;",
-      "INSERT into scoreboard (userName, score) values ('test', 123123)",
+      "SELECT userName, score FROM scoreboard order by score DESC;",
+      // "INSERT into scoreboard (userName, score) values ('test', 123123)",
       [req.body.name, req.body.score],
       (error, results) => {
         if (error) {
